22acd3f24c069cd27291d4f2a9a73ee138ecfdc8
Carrye Gaulin <carryegaulin033178@gmail.com>
2020-12-14 21:21:44 -0500

    Carrye add

---
+++ components/Carrye.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default () => (
+  <div className="page">
+    <div className="title">Carrye Gaulin</div>
+    <div className="subtitle">I am an aspiring software engineer and web developer. I cannot stand the cold and I am ready for some sunshine!</div>
+  </div>
+)
---
+++ index.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { render } from 'react-dom'
+import Carrye from './components/Carrye'
+
+render(
+  <Carrye />,
+  document.getElementById('root'),
+)
